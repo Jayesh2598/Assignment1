9b197ddc8942eaa81d853a0a220cb0ffea934558
Jay0002 <jayesh2598@gmail.com>
2020-09-22 15:54:23 +0530

    Checking if Employee is present or absent

--- EWCP.java
+++ EWCP.java
@@ -5,6 +5,12 @@ public class EWCP {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Programme!");
+		
+		int att= (int) (Math.floor(Math.random()*10) %2);
+		if(att == 1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
