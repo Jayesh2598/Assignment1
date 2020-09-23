5646c76482d822d35710369aa53c327a72aeb08b
Jayesh2598 <71629908+Jayesh2598@users.noreply.github.com>
2020-09-23 15:37:32 +0530

    Daily employee wage calculated

--- EWCP.java
+++ EWCP.java
@@ -7,7 +7,6 @@ public class EWCP {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		int DAILY_EMP_WAGE= WAGE_PER_HOUR * FULL_DAY_HOUR;
 		System.out.println("The daily wage of the employee is :" + DAILY_EMP_WAGE);
 	}
