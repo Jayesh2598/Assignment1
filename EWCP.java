db3f21118fe5345a13d6939f7b0f65d099bba613
Jay0002 <jayesh2598@gmail.com>
2020-09-23 15:58:06 +0530

    Adding part time employee and wages

--- EWCP.java
+++ EWCP.java
@@ -2,14 +2,32 @@ package assignment1;
 
 public class EWCP {
 	
-	public static int WAGE_PER_HOUR=20;
-	public static int FULL_DAY_HOUR=8;
+	public static final int WAGE_PER_HOUR= 20;
+	public static final int PART_TIME= 1;
+	public static final int FULL_DAY= 2;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int DAILY_EMP_WAGE= WAGE_PER_HOUR * FULL_DAY_HOUR;
-		System.out.println("The daily wage of the employee is :" + DAILY_EMP_WAGE);
+		int empHours=0;
+		int empWage=0;
+		int empType = (int) (Math.floor(Math.random() * 10) % 3);
+		
+		if(empType== FULL_DAY) {
+			System.out.println("Full time employee.");
+			empHours=8;
+		}	
+		else if(empType==PART_TIME) {
+			System.out.println("Part time employee.");
+			empHours=4;
+		}	
+		else {
+			System.out.println("Employee absent.");
+			empHours=0;
+		}
+		
+		empWage= WAGE_PER_HOUR * empHours;
+		System.out.println("The daily wage of the employee is :" + empWage);
 	}
 
 }
